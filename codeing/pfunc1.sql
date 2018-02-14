25075b8248bbff7bc79183e92a833f2a5800cb2b
thurbakt2 <thurbakt@gmail.com>
2018-02-14 08:02:04 -0700

    Add files via upload

---
+++ codeing/pfunc1.sql
@@ -0,0 +1,54 @@
+insert into ihc_last_cust_id values ('5','01-Mar-2017');
+
+insert into ihc_orders values ('1','01-Jan-2010','10',5);
+insert into ihc_orders values ('1','01-Jan-2010','11',1);
+insert into ihc_orders values ('2','01-Jan-2010','12',25.7);
+insert into ihc_orders values ('3','01-Jan-2010','1',9999);
+
+insert into ihc_org_contact values ('1','1699','31-Jan-2017','Fred Smith','Salesman',1,9,'Fred@yahoo.com','1','31-Jan-2017','athurb');
+insert into ihc_org_contact values ('3','1699','31-Jan-2017','Fred Johns','Mgr',2,1,'FredJ@yahoo.com','2','31-Jan-2017','athurb');
+
+insert into ihc_names values ('1','01-Feb-2010','John Doe','JD','Mr','Jr','General Mgr ','Doe','John',' ','Mr John Doe Jr','31-Jan-2017','athurb');
+insert into ihc_names values ('2','02-Feb-2010','Jim Doe','JD','Mr',' ','General Mgr ','Doe','Jim',' ','Mr Jim Doe','31-Jan-2017','athurb');
+insert into ihc_names values ('3','03-Feb-2010','Sally Doe','JD','Ms',' ','General Mgr ','Doe','Sally',' ','Mr Sally Doe','31-Jan-2017','athurb');
+insert into ihc_names values ('4','04-Feb-2010','Fred Doe','JD','Mr',' ','General Mgr ','Doe','Fred',' ','Mr Fred Doe','31-Jan-2017','athurb');
+insert into ihc_names values ('5','05-Feb-2010','Hank Doe','JD','Mr',' ','General Mgr ','Doe','Hank',' ','Mr Hank Doe','31-Jan-2017','athurb');
+
+insert into ihc_customer values ('1','A','01-Jan-2010','01-Jan-2019','x','01-Jan-2017','03-Mar-2017','ABC Titles','N');
+insert into ihc_customer values ('2','A','01-Jan-2010','01-Feb-2019','x','01-Jan-2017','03-Mar-2017','ABCD Titles','N');
+insert into ihc_customer values ('3','A','01-Jan-2010','01-Jan-2019','x','01-Jan-2017','03-Mar-2017','ABCDE Titles','N');
+insert into ihc_customer values ('4','A','01-Jan-2010','01-Jan-2019','x','01-Jan-2017','03-Mar-2017','ABCDEF Titles','N');
+insert into ihc_customer values ('5','A','01-Jan-2010','01-Jan-2019','x','01-Jan-2017','03-Mar-2017','ABCDEFG Titles','N');
+
+insert into ihc_cust_address values ('1','01-Feb-2010','A','123 West 400 N',' ',' ',' ','Ogden','UT','84404');
+insert into ihc_cust_address values ('2','01-Feb-2010','A','1232 West 200 N',' ',' ',' ','Ogden','UT','84404');
+insert into ihc_cust_address values ('3','01-Feb-2010','A','1233 West 300 N',' ',' ',' ','Ogden','UT','84404');
+insert into ihc_cust_address values ('4','01-Feb-2010','A','1234 West 400 N',' ',' ',' ','Ogden','UT','84404');
+insert into ihc_cust_address values ('5','01-Feb-2010','A','1235 West 500 N',' ',' ',' ','Ogden','UT','84404');
+
+insert into ihc_client_rep values ('16970','01-Jan-2010','Bud Hill',' BH',' ',' ','Client rep 1','Hill','Bud',' ',' ',sysdate,'athurb');
+insert into ihc_client_rep values ('01681','01-Jan-2010','Yvete Hill',' YH',' ',' ','Client rep 2','Hill','Yvete',' ',' ',sysdate,'athurb');
+insert into ihc_client_rep values ('07588','01-Jan-2010','Jim Hill',' JH',' ',' ','Client rep 3','Hill','Jim',' ',' ',sysdate,'athurb');
+insert into ihc_client_rep values ('05015','01-Jan-2010','Fred Hill',' FH',' ',' ','Client rep 4','Hill','Fred',' ',' ',sysdate,'athurb');
+
+insert into ihc_client_assignments values ('16970','01-Jan-2010','1','01-Jan-2017','athurb');
+insert into ihc_client_assignments values ('01681','01-Jan-2010','4','01-Jan-2017','athurb');
+insert into ihc_client_assignments values ('05015','01-Jan-2010','2','01-Jan-2017','athurb');
+insert into ihc_client_assignments values ('07588','01-Jan-2010','3','01-Jan-2017','athurb');
+
+insert into ihc_work_phone values ('1','W','80171081638',' ','Y');
+insert into ihc_work_phone values ('2','W','80171081638',' ','Y');
+insert into ihc_work_phone values ('2','C','80171081234',' ','N');
+insert into ihc_work_phone values ('3','C','80171081638',' ','Y');
+insert into ihc_work_phone values ('4','W','80171081638',' ','Y');
+insert into ihc_work_phone values ('5','O','80171081638',' ','Y');
+
+insert into ihc_products values ('12','01-Jan-2010','Thing1','This thing if great!');
+insert into ihc_products values ('11','01-Jan-2010','Thing2','This thing if great! ');
+insert into ihc_products values ('10','01-Jan-2010','Thing3','This thing if great! ');
+insert into ihc_products values ('1','01-Jan-2010','Thing4','This thing if great! ');
+
+insert into ihc_costs values ('12','01-Jan-2010',25.75);
+insert into ihc_costs values ('11','01-Jan-2010',0.75);
+insert into ihc_costs values ('10','01-Jan-2010',125.754);
+insert into ihc_costs values ('1','01-Jan-2010',5.75);
